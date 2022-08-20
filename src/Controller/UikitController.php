0ee9d48433cce85951d378860ad6e01fc6a951ed
YOUPIDOK <58979097+YOUPIDOK@users.noreply.github.com>
2022-08-20 21:08:59 +0200

    Update security uikit page

--- src/Controller/UikitController.php
+++ src/Controller/UikitController.php
@@ -4,13 +4,18 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Annotation\Route;
 
 class UikitController extends AbstractController
 {
     #[Route('/uikit', name: 'uikit')]
-    public function uikit(): Response
+    public function uikit(): NotFoundHttpException|Response
     {
+        if ($this->getParameter('kernel.environment') === 'prod') {
+            throw new NotFoundHttpException();
+        }
+
         $this->addFlash('info', 'info');
         $this->addFlash('success', 'succsess');
         $this->addFlash('warning', 'warning');
